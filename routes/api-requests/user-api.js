1cf08a4477da8425d47b348d6a55cd63483a82b8
Your Name <you@example.com>
2019-09-11 20:20:09 -0700

    bug fix

--- routes/api-requests/user-api.js
+++ routes/api-requests/user-api.js
@@ -1,9 +1,3 @@
-const firebaseInt = require('../../assets/service_account');
-const admin = require('firebase-admin');
-
-
-const db = admin.firestore();
-
 module.exports = ( app, protect ) => {
     app.get('/get/projects/', async (req, res) => {
         //
